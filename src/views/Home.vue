ffc2957b0c50dc4a5e15c1eb1dd5940a6f21bf63
Stephon Smith <smithwrestling89@gmail.com>
2021-09-10 23:26:26 -0400

    Centered header and button. Added styling to match theme.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,26 @@
 <template>
-  <p>Home!</p>
+  <div class="hero-container">
+        <video :src="yoga2" preload="auto" autoplay loop muted playsInline type="video/mp4" alt="A woman performing yoga on a sand dune whilst facing a city in the distance."/>
+        <h1>FIT UNIVERSE</h1>
+        <div class="hero-btns">
+        <router-link :to="{ name: 'Login' }">
+          <button>
+                <p>GET STARTED</p>
+          </button>
+        </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
+import yoga2 from '../assets/videos/yoga2.mp4'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+
+    return { yoga2 }
+  }
 }
 </script>
+ 
