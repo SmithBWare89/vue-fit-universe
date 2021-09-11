fc1d21063ff473bf62110c35375ed4681fd89174
Stephon Smith <smithwrestling89@gmail.com>
2021-09-11 13:37:46 -0400

    added flex direction for navbar and links as well as styling for hovering on links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,29 @@
 <template>
     <nav>
-        <div>
+        <div class="nav-header">
             <h1>Fit Universe</h1>
         </div>
-        <div>
-            <i class="fas fa-user"></i>
-            <i class="fas fa-home"></i>
-            <i class="fas fa-dumbbell"></i>
-            <i class="fas fa-music"></i>
-            <i class="fas fa-shopping-cart"></i>
+        <div class="nav-links">
+            <span class="nav-icons">
+                <i class="fas fa-user"></i>
+                <p>User</p>
+            </span>
+            <span class="nav-icons">
+                <i class="fas fa-home"></i>
+                <p>Home</p>
+            </span>
+            <span class="nav-icons">
+                <i class="fas fa-dumbbell"></i>
+                <p>Workouts</p>
+            </span>
+            <span class="nav-icons">
+                <i class="fas fa-music"></i>
+                <p>Music</p>
+            </span>
+            <span class="nav-icons">
+                <i class="fas fa-shopping-cart"></i>
+                <p>Shop</p>
+            </span>
         </div>
     </nav>
     
@@ -24,6 +39,42 @@ export default {
 
 nav {
     width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: var(--beau);
+    font-family: 'Fira Sans', sans-serif;
+}
+
+.nav-header {
+    margin: 0px 0px 0px 10px;
+}
+
+.nav-header h1 {
+    margin: 0;
+    font-size: 24px;
+}
+
+.nav-links {
+    display: flex;
+    align-items: center;
+}
+
+.nav-links i {
+    margin: 5px 5px;
+}
+
+.nav-icons {
+    display: flex;
+    align-items: center;
+    margin: 0 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+.nav-icons:hover {
+    color: var(--white);
+    background: var(--pewter);
 }
 
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,8 +7,6 @@
 
 <script>
 import NavBar from '../components/NavBar.vue'
-import { useRoute } from 'vue-router'
-import { ref } from '@vue/reactivity'
 export default {
     name: 'Dashboard',
     components: { NavBar }
