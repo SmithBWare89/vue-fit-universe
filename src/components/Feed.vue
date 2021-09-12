896dae44e958c1b1d0d5589d4afe7f6088f2a6c3
Stephon Smith <smithwrestling89@gmail.com>
2021-09-12 11:37:11 -0400

    added initial feed element

---
+++ src/components/Feed.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="feed-container">
+        <h1>Hello</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Feed'
+}
+</script>
+
+<style>
+    .feed-container {
+        width: 85%;
+        background: var(--beau);
+        margin: 0 auto;
+        border-radius: 30px;
+        padding: 10px;
+        margin-top: 10px;
+    }
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,14 +2,16 @@
     <NavBar />
     <div class="app-content">
         <!-- App Components Will Go here -->
+        <Feed />
     </div>
 </template>
 
 <script>
 import NavBar from '../components/NavBar.vue'
+import Feed from '../components/Feed.vue'
 export default {
     name: 'Dashboard',
-    components: { NavBar }
+    components: { NavBar, Feed }
 }
 </script>
 
